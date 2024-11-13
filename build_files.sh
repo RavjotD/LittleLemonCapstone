4abba10623c4b8c0a226db71c42f53b60d7e7347
Ravjot Duhra <duhraravjot@gmail.com>
2024-11-12 20:31:25 -0800

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-# Exit on first error
-set -e
+# Activate virtual environment
+source .venv/bin/activate
 
 # Install dependencies
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
 
 # Collect static files
-python3 manage.py collectstatic --noinput
-
-# Ensure static output directory exists
-mkdir -p .vercel/output/static
-
-# Copy static files
-cp -r static/ .vercel/output/static/
-
-# Make migrations
-python3 manage.py makemigrations
-
-# Apply migrations
-python3 manage.py migrate
+python manage.py collectstatic --noinput
