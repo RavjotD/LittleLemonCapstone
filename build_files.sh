4172d06348667e5bca25f7f0e48cad37d4372ef5
Ravjot Duhra <duhraravjot@gmail.com>
2024-11-12 18:07:40 -0800

    updated

--- build_files.sh
+++ build_files.sh
@@ -4,10 +4,11 @@
 set -e
 
 # Install dependencies
-pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 # Collect static files
-python3.12 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Ensure static output directory exists
 mkdir -p .vercel/output/static
@@ -16,7 +17,7 @@ mkdir -p .vercel/output/static
 cp -r static/ .vercel/output/static/
 
 # Make migrations
-python3.12 manage.py makemigrations
+python3 manage.py makemigrations
 
 # Apply migrations
-python3.12 manage.py migrate
+python3 manage.py migrate
