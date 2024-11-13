2dfa57552c230a843a5fc40ece66b34002cf4238
Ravjot Duhra <duhraravjot@gmail.com>
2024-11-13 15:17:52 -0800

    updated

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pq+*sfm4=0iaqasbttex^2kwzrou4txmcue1gpc34$)+z11pfp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh', '127.0.0.1' ]
 
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
