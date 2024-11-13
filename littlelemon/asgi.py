fe6a9381aff2dfe699807a68186b7bfe6fb64824
Ravjot Duhra <duhraravjot@gmail.com>
2024-11-12 16:36:59 -0800

    updated

--- littlelemon/asgi.py
+++ littlelemon/asgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'littlelemon.settings')
 
-application = get_asgi_application()
+app = get_asgi_app()
--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'littlelemon.wsgi.application'
+WSGI_APPLICATION = 'littlelemon.wsgi.app'
 
 
 # Database
