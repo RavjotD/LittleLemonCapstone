7cb4795b050cc48cd533c3fee88391bd1f15562e
Ravjot Duhra <duhraravjot@gmail.com>
2024-11-12 17:21:19 -0800

    updated

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pq+*sfm4=0iaqasbttex^2kwzrou4txmcue1gpc34$)+z11pfp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1' ]
+ALLOWED_HOSTS = ['.vercel.app','.now.sh', '127.0.0.1' ]
 
 
 # Application definition
