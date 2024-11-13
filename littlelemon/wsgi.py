a0ffa700e7d5742964248a4e94212f21a133cf64
Ravjot Duhra <duhraravjot@gmail.com>
2024-11-12 16:44:03 -0800

    updated

--- littlelemon/wsgi.py
+++ littlelemon/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_app
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'littlelemon.settings')
 
-app = get_wsgi_app()
+app = get_wsgi_application()
