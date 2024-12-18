d4cce4bfe5f86d015562e8651e3285a21affa003
Ravjot Duhra <duhraravjot@gmail.com>
2024-12-18 15:25:17 -0800

    updated deployment

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['.amplifyapp.com', '127.0.0.1', ]
 
 
 # Application definition
